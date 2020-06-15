701583b76926a9b24bf6c96e3ab3aca161b1bf0e
vishwasc20 <vishwasc20@gmail.com>
2020-06-15 21:31:43 +0530

    tweaks

--- src/webpack.client.js
+++ src/webpack.client.js
@@ -10,67 +10,84 @@ console.log('client webpack env...', nodeEnv);
 console.log('client webpack isProduction...', isProduction);
 
 const clientConfig = {
-  ...baseConfig,
-  ...{
-    mode: nodeEnv,
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    entry: {
-      client: './containers/ClientApp.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'public/assets'),
-      // https://github.com/gregberge/loadable-components/issues/348
-      // https://github.com/webpack/webpack/issues/443#issuecomment-54113862
-      // https://webpack.js.org/guides/public-path/
-      // to request client bundles with correct exposed public path
-      // should match with express static pulbic path
-      publicPath: '/assets/'
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          reactmaterial: {
-            test: /[\\/]node_modules[\\/](react|react-dom|@material-ui)[\\/]/,
-            name: 'react-material',
-            chunks: 'all',
-            priority: 30
-          },
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendor',
-            chunks: 'all',
-            priority: 20
-          },
-          // https://itnext.io/react-router-and-webpack-v4-code-splitting-using-splitchunksplugin-f0a48f110312
-          common: {
-            name: 'common',
-            minChunks: 2,
-            chunks: 'all',
-            priority: 10,
-            reuseExistingChunk: true,
-            enforce: true
-          }
+  mode: nodeEnv,
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  entry: {
+    client: './containers/ClientApp.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        // Should be after babel-loader
+        test: /\.js$/,
+        loader: 'stylelint-custom-processor-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/assets'),
+    // https://github.com/gregberge/loadable-components/issues/348
+    // https://github.com/webpack/webpack/issues/443#issuecomment-54113862
+    // https://webpack.js.org/guides/public-path/
+    // to request client bundles with correct exposed public path
+    // should match with express static pulbic path
+    publicPath: '/assets/'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        reactmaterial: {
+          test: /[\\/]node_modules[\\/](react|react-dom|@material-ui)[\\/]/,
+          name: 'react-material',
+          chunks: 'all',
+          priority: 30
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: 20
+        },
+        // https://itnext.io/react-router-and-webpack-v4-code-splitting-using-splitchunksplugin-f0a48f110312
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true
         }
       }
-    },
-    plugins: [
-      // Force imports of packages like @material-ui/core to use the /es versions
-      new webpack.NormalModuleReplacementPlugin(
-        /^@material-ui\/core(\/|$)/,
-        (resource) => {
-          // eslint-disable-next-line no-param-reassign
-          resource.request = resource.request.replace(
-            /^(@[^/+]+\/[^/+]+|[^/+]+)(?:\/esm)?(\/.*)?$/,
-            '$1/esm$2'
-          );
-        }
-      )
-    ]
-  }
+    }
+  },
+  plugins: [
+    // Force imports of packages like @material-ui/core to use the /es versions
+    new webpack.NormalModuleReplacementPlugin(
+      /^@material-ui\/core(\/|$)/,
+      (resource) => {
+        // eslint-disable-next-line no-param-reassign
+        resource.request = resource.request.replace(
+          /^(@[^/+]+\/[^/+]+|[^/+]+)(?:\/esm)?(\/.*)?$/,
+          '$1/esm$2'
+        );
+      }
+    )
+  ]
 };
 
 module.exports = clientConfig;
--- src/webpack.server.js
+++ src/webpack.server.js
@@ -36,32 +36,49 @@ const serverModule = {
 // Note that since this is for the server, it is important to
 // set the target to node and set the libraryTarget to commonjs2
 const serverConfig = {
-  ...baseConfig,
-  ...{
-    mode: nodeEnv,
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    target: 'node',
-    module: serverModule,
-    entry: {
-      server: './containers/ServerApp.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, './build'),
-      libraryTarget: 'commonjs2'
-    },
-    externals: [nodeExternals()],
-    plugins: [
-      new webpack.BannerPlugin({
-        banner: 'require("source-map-support").install();',
-        raw: true,
-        entryOnly: false
-      }),
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1
-      })
+  mode: nodeEnv,
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  target: 'node',
+  module: serverModule,
+  entry: {
+    server: './containers/ServerApp.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        // Should be after babel-loader
+        test: /\.js$/,
+        loader: 'stylelint-custom-processor-loader',
+        exclude: /node_modules/
+      }
     ]
-  }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './build'),
+    libraryTarget: 'commonjs2'
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
 };
 
 // https://medium.com/@glennreyes/how-to-disable-code-splitting-in-webpack-1c0b1754a3c5
